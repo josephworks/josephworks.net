0dd3d82b711872624494c9d1804335389e7cba83
Joseph Robinson <smartech56@gmail.com>
2020-05-01 11:00:46 -0700

    added loading functions

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+    alert('Page is loaded!');
+});
+
+window.onload = function() {
+    alert("Please note that this site is still in development!");
+};
