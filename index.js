ace0f40d9f5c14f4d0d64a0edd2a3eaec53dde89
Joseph Robinson <smartech56@gmail.com>
2020-05-01 11:20:00 -0700

    Added console.logs

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 window.addEventListener("load", function() {
     alert('Page is loaded!');
+    console.log('Page is loaded!')
 });
 
 window.onload = function() {
     alert("Please note that this site is still in development!");
+    console.log('Please note that this site is still in development!')
 };
+
+document.getElementById('hello').textContent = 'Hello, Console!'
